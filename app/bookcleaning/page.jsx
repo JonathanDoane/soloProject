e65b965f9af5b3e5c87b75578650de78d8be485d
Jonathan Doane <johnnydoane13@gmail.com>
2023-08-07 18:51:57 -0700

    success on session

---
+++ app/bookcleaning/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function BookCleaning() {
+
+    return(
+        <div>
+            <Link href={'/home'}>Home</Link>
+            <Link href={'/rates'}>View Standard Rates</Link>
+        </div>
+    )
+}
